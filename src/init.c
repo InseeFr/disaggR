6b30e50baa574f19a1da0853996cfbb93836bbe1
arnaud-feldmann <60670870+arnaud-feldmann@users.noreply.github.com>
2021-01-12 15:14:48 +0100

    Update init.c

--- src/init.c
+++ src/init.c
@@ -1,13 +1,8 @@
 #include <R.h>
 #include <Rinternals.h>
-#include <stdlib.h> // for NULL
+#include <stdlib.h>
 #include <R_ext/Rdynload.h>
 
-/* FIXME: 
-   Check these declarations against the C/Fortran source code.
-*/
-
-/* .Call calls */
 extern SEXP Cpp_window(SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
