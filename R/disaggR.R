95ed4086ab6296bb89794475937e63f39ac0d9f3
Thomas LAURENT <83507627+ThomasLaurentInsee@users.noreply.github.com>
2021-12-06 23:23:11 +0100

    Correction rePort avec parametres de date (#57)
    * Correction rePort avec parametres de date
    * mise à jour NEWS
    * ajout lien vers IM 126 dans la description et disaggR.R
    
    Co-authored-by: Laurent Thomas <P8QOP2@ad.insee.intra>
    Co-authored-by: arnaud <arnaud.feldmann@gmail.com>

--- R/disaggR.R
+++ R/disaggR.R
@@ -8,10 +8,11 @@
 #' resulting time-serie is strictly equal to the low-frequency serie within the 
 #' benchmarking window. Typically, the low-frequency serie is an annual one, 
 #' unknown for the last year, and the high frequency one is either quarterly or 
-#' mensual.
+#' monthly.
 #' 
 #' See "Methodology of quarterly national accounts", Insee Méthodes 
-#' N°126, by Insee (2012, ISBN:978-2-11-068613-8).
+#' N°126, by Insee (2012, ISBN:978-2-11-068613-8,
+#' https://www.insee.fr/en/information/2579410).
 #'
 #' @import stats graphics
 #' @keywords internal
