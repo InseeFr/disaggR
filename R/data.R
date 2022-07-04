22aa9a848b8e5c6b3c5b81037f8b10b6e3743e4c
Laurent Thomas <P8QOP2@ad.insee.intra>
2022-07-04 17:57:38 +0200

    Initilaisation vignette

--- R/data.R
+++ R/data.R
@@ -19,3 +19,25 @@
 #' @source \url{https://www.insee.fr/fr/statistiques/4636032}
 #' @keywords internal
 "turnover"
+
+#' Total consumption in accommodation and food services at current prices
+#'
+#' An annual time series containing the consumption in accommodation and food services services from 1999
+#' to 2021, as retrieved the 04/07/2022.
+#'
+#' @format A time series of frequency 1
+#' 
+#' @source \url{https://www.insee.fr/en/statistiques/serie/010548586}
+#' @keywords internal
+"consumption_catering"
+
+#' Turnover indicator in accommodation and food services
+#'
+#' An monthly time series containing the turnover indicator in accommodation and food services
+#' from january 1999 to april 2022, as retrieved the 04/07/2022.
+#'
+#' @format A time series of frequency 12
+#' 
+#' @source \url{https://www.insee.fr/en/statistiques/serie/010543406}
+#' @keywords internal
+"turnover_catering"
